47d7442eb06aa33fa05d54c12a4d8ecc00875a37
monesonn <monesonn@tuta.io>
2021-02-20 19:36:23 +0200

    update script

--- yata.sh
+++ yata.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
-
 # Author : Stanley <git.io/monesonn>
 
 # Script version
-__version="0.1.0-rc1"
+__version="0.1.0-rc2"
+
+# General variables
+
+# quiet mode is the default mode
+# ISQUIET="-q --console-title"
+ISQUIET=""
+VERBOSE=0
+
+DIR=''
+YTDL_BIN=`which youtube-dl`
+DEFAULT_DIR="$HOME/Music"
+PLAYLIST_DIR="${DEFAULT_DIR}/playlists"
+UPLOADER_DIR="${DEFAULT_DIR}/uploader"
+YTDL_OUPUT_TEMPLATE='%(playlist_index)s - %(title)s.%(ext)s'
+YTDL_FORMAT='mp3'
 
 # Some variables to initialize colors for colorfull echo output
 BLUE='\033[1;34m'
 RED='\033[31m'
 CYAN='\033[1;36m'
 NC='\033[0m'
-
-# Variables
-
-DIR=''
-PLAYLIST_DIR='$HOME/Music/playlists/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'
 
 _help() {
 cat << EOF
@@ -39,24 +48,36 @@ cat << EOF
 EOF
 }
 
+download_playlist() {
+  $YTDL_BIN \
+  ${ISQUIET} \
+  --embed-thumbnail \
+  --ignore-errors \
+  --extract-audio \
+  --no-overwrites \
+  --audio-format $YTDL_FORMAT \
+  --output "${DEFAULT_DIR}/playlists/%(playlist)s/${YTDL_OUPUT_TEMPLATE}" \
+  "$1"
+}
+
 err_msg() { echo -e "${RED}$1${NC}"; }
 
 __main__() {
   case $# in
   0) _help ; exit 1 ;;
   1) case $1 in
-      -h | --help | help) _help ;;
-      -v | --version | version) printf "$__version\n" ;;
+      -h | --help | help) shift; _help ;;
+      -v | --version | version) shift; printf "$__version\n" ;;
       # -p | --playlist | playlist)  ;;
       *) err_msg "No such option: $1" ; exit 1 ;;
     esac;;
   2) 
     if [[ "$1" == "-p" || "$1" == "--playlist" || "$1" == "playlist" ]]; then
-      youtube-dl --embed-thumbnail --ignore-errors --extract-audio --audio-format mp3 --output "$PLAYLIST_DIR" "$2"
+      download_playlist "$2"
     else err_msg "Input error." ; exit 1
     fi;;  
   *) err_msg "Input error, too many arguments.\nType yata [-h|--help|help] to see a list of all options." ; exit 1 ;;
   esac
 }
 
-__main__ "$@"
+__main__ "$@" ; exit 0
