9fd2e4bdaee211b74f3dd540d8bd7944d3fdb230
monesonn <monesonn@tuta.io>
2021-02-23 17:33:54 +0200

    merge two function into one, another minor changes..

--- yta.sh
+++ yta.sh
@@ -2,34 +2,30 @@
 # Author : Stanley <git.io/monesonn>
 
 # Script version
-__version="0.2.1"
+__version="0.2.2"
 
-# General variables
+# GENERAL
 
-# quiet mode is the default mode
-IS_QUIET=''
-
+## Default directories
 DEFAULT_DIR="$HOME/Music"
 PLAYLIST_DIR="${DEFAULT_DIR}/playlists"
 UPLOADER_DIR="${DEFAULT_DIR}/uploader"
-
+## Output Template
 OUTPUT='%(uploader)s - %(title)s [%(id)s].%(ext)s'
-
-# Default
+## Default variables
+QUIET=''
 AUDIO_EXT='mp3'
 BITRATE='128K'
 SAMPLE_RATE='48000'
 PLAYLIST=false
 SOX=false
-
-# Some variables to initialize colors for colorfull echo output
+## Some variables to initialize colors for colorfull echo output
 DARK_BLUE='\033[34m'
 RED='\033[31m'
 BLUE='\033[1;34m'
 NC='\033[0m'
 
 _help() {
-
 echo -ne \
 "${DARK_BLUE}
  ▀▄▀ ▄▀▄ ▀█▀ ▄▀▄
@@ -65,81 +61,72 @@ cat << EOF
 EOF
 }
 
+err_msg() { echo -e "${RED}$1${NC}"; }
+
 download() {
   [[ ${AUDIO_EXT} = mp3 ]] && local EMBED="--embed-thumbnail" || local EMBED="" 
-
   echo "[yata] Starting..."
-  youtube-dl \
-  ${IS_QUIET} \
-  --format "bestaudio[asr = ${SAMPLE_RATE}]" \
-  --ignore-errors \
-  --no-continue \
-  --no-overwrites \
-  --add-metadata \
-  --extract-audio \
-  --audio-format ${AUDIO_EXT} \
-  --audio-quality ${BITRATE} \
-  ${EMBED} \
-  --metadata-from-title "(?P<artist>.+?) - (?P<title>.+)" \
-  --output "${DEFAULT_DIR}/${AUDIO_EXT}/%(title)s.%(ext)s" \
-  --exec 'echo [yata] {} is downloaded.' \
-  $1 `# URL` 2>/dev/null
-  echo "[yata] All is done."
-  exit 0
-}
-
-download_playlist() {
-  local PLAYLIST_TITLE=`youtube-dl --no-warnings --dump-single-json $1 | jq -r '.title'`
-
-  echo "[yata]: Starting to download playlist \"${PLAYLIST_TITLE}\""
-  youtube-dl \
-  ${IS_QUIET} \
-  --format "bestaudio[asr = ${SAMPLE_RATE}]" \
-  --ignore-errors \
-  --no-continue \
-  --no-overwrites \
-  --add-metadata \
-  --yes-playlist \
-  --extract-audio \
-  --audio-format ${AUDIO_EXT} \
-  --audio-quality ${BITRATE} \
-  --embed-thumbnail \
-  --metadata-from-title "(?P<title>.+)" \
-  --output "${PLAYLIST_DIR}/%(playlist)s/%(playlist_index)s %(title)s.%(ext)s" \
-  --exec 'echo [yata] {} is downloaded.' \
-  $1 `# URL` 2>/dev/null
-  if [ ${SOX} = true ] ; then
+  if [ $PLAYLIST = true ] ; then
+    local PLAYLIST_TITLE=`youtube-dl --no-warnings --dump-single-json $1 | jq -r '.title'`
+    echo "[yata] Playlist \"${PLAYLIST_TITLE}\" is downloading."
+    youtube-dl \
+    ${QUIET} \
+    --format "bestaudio[asr = ${SAMPLE_RATE}]" \
+    --ignore-errors \
+    --no-continue \
+    --no-overwrites \
+    --add-metadata \
+    --yes-playlist \
+    --extract-audio \
+    --audio-format ${AUDIO_EXT} \
+    --audio-quality ${BITRATE} \
+    ${EMBED} \
+    --metadata-from-title "(?P<title>.+)" \
+    --output "${PLAYLIST_DIR}/%(playlist)s/%(playlist_index)s %(title)s.%(ext)s" \
+    --exec 'echo [yata] {} is downloaded.' \
+    $1 `# URL` 2>/dev/null
     # lmao, idk, but it's works 
-
-    # files=${PLAYLIST_DIR}/${playlist_title}/*.${AUDIO_EXT}
-    echo "[sox]  Starting to merge ${PLAYLIST_TITLE}."
-    sox "${PLAYLIST_DIR}/${PLAYLIST_TITLE}/*.${AUDIO_EXT}" "${DEFAULT_DIR}/${AUDIO_EXT}/${PLAYLIST_TITLE}.${AUDIO_EXT}"
-    echo "[sox]  ${DEFAULT_DIR}/${AUDIO_EXT}/${PLAYLIST_TITLE}.${AUDIO_EXT} is merged."
+    [[ $SOX = true ]] && echo "[sox]  Starting to merge ${PLAYLIST_TITLE}." \
+    sox "${PLAYLIST_DIR}/${PLAYLIST_TITLE}/*.${AUDIO_EXT}" "${DEFAULT_DIR}/${AUDIO_EXT}/${PLAYLIST_TITLE}.${AUDIO_EXT}" \
+    echo "[sox]  ${DEFAULT_DIR}/${AUDIO_EXT}/${PLAYLIST_TITLE}.${AUDIO_EXT} is merged." 
+    exit 0
+  else
+    youtube-dl \
+    ${QUIET} \
+    --format "bestaudio[asr = ${SAMPLE_RATE}]" \
+    --ignore-errors \
+    --no-continue \
+    --no-overwrites \
+    --add-metadata \
+    --extract-audio \
+    --audio-format ${AUDIO_EXT} \
+    --audio-quality ${BITRATE} \
+    ${EMBED} \
+    --metadata-from-title "(?P<artist>.+?) - (?P<title>.+)" \
+    --output "${DEFAULT_DIR}/${AUDIO_EXT}/%(title)s.%(ext)s" \
+    --exec 'echo [yata] {} is downloaded.' \
+    $1 `# URL` 2>/dev/null
+    exit 0
   fi
   echo "[yata] All is done."
-  exit 0
 }
-
-err_msg() { echo -e "${RED}$1${NC}"; }
 
 __main__() {
   while [[ "$#" -gt 0 ]]; do
     argument="$1"
     case $argument in
-      -p | --playlist | playlist) PLAYLIST=true ; shift 2 ;;
       -a=* | --audio=* | audio=*) AUDIO_EXT="${argument#*=}" ; shift ;;
       -b=* | --bitrate=* | bitrate=*) BITRATE="${argument#*=}" ; shift ;;
-      -q | --quiet | quiet) IS_QUIET='--quiet --console-title' ; shift ;;
+      -q | --quiet | quiet) QUIET='--quiet --console-title' ; shift ;;
       -1 | --sox | sox) SOX=true ; shift ;; 
+      -p | --playlist | playlist) PLAYLIST=true ; shift ;;
       -v | --version | version) printf "$__version\n" ; exit 0 ;;
       -h | --help | help) _help ; exit 0 ;;
       -* | --*) err_msg "No such option: $argument.\nType yta [-h|--help|help] to see a list of all options." ; exit 1 ;;
-      *) [[ ${PLAYLIST} = false ]] && download $argument || download_playlist $argument ;;
+      *) download $argument ;;
     esac
   done
+  err_msg "Something went wrong..." ; exit 1
 }
 
-if [[ ${#} -eq 0 ]]; then
-  _help ; exit 1
-fi
-__main__ "$@" ; exit 0
+[[ ${#} -eq 0 ]] && _help || __main__ "$@" 
